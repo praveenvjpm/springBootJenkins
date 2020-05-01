5110bda1eb3a3c2f8605ed8997141408e86114e1
praveenvjpm <praveenvjpm@gmail.com>
2020-05-01 22:29:51 +0530

    Initialized project with Controller

---
+++ src/test/java/in/praveen/SpringBootJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package in.praveen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
